acc1ab921fce4bdeb3278742d1e845a65ccb8f9a
Unknown <felipe.silva@cern.ch>
2017-09-19 13:16:30 +0200

    das

--- run_Eff_JetHT.C
+++ run_Eff_JetHT.C
@@ -7,8 +7,9 @@
 	gStyle->SetOptStat(0);
 
 	char inpath[200];
-	sprintf(inpath, "data/ggtree_data_*.root");
-	
+	// sprintf(inpath, "data/ggtree_data_*.root");
+	sprintf(inpath, "/eos/uscms/store/user/ftorresd/triggerStudies2017/JetHT_Run2017D-PromptReco-v1_ggNtuples_JetHT_Run2017D_PromptReco_v1_DCSONLY_v11/JetHT/JetHT_Run2017D-PromptReco-v1/170917_183228/000*/*.root");
+
 
   // xEff_JetHT(inpath, 1);
 	xEff_JetHT(inpath);
